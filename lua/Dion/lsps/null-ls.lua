c8cca80bbb5b694a124c6dd2129acb2605e94140
khanhanhtr <khanhanhtr00@gmail.com>
2023-03-15 22:58:07 +1100

    Missing treesitter, complete lsp support

--- lua/Dion/lsps/null-ls.lua
+++ lua/Dion/lsps/null-ls.lua
@@ -14,16 +14,16 @@ null_ls.setup({
 		-- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
 		formatting.black.with({ extra_args = { "--fast" } }), --pip3 install
 		formatting.stylua, --cargo install
-                formatting.goline -- go install github.com/segmentio/golines@latest
-                formatting.terra_fmt --install with terraform
+                formatting.goline,   -- go install github.com/segmentio/golines@latest
+                formatting.terra_fmt,  --install with terraform
 		diagnostics.flake8, --pip3 install
-                diagnostics.goclangci_lint --curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.51.2
-                diagnostics.ansiblelint  --pip3 install 
-                diagnostics.terraform_validate --install with terraform
+                diagnostics.goclangci_lint,  --curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.51.2
+                diagnostics.ansiblelint,   --pip3 install 
+                diagnostics.terraform_validate,  --install with terraform
                 formatting.beautysh -- pip3 install 
                 diagnostics.eslint_d.with({
       filetypes={"vue"}
-    }) --npm install , quite good to use with vue
+    }),  --npm install , quite good to use with vue
                   
                 formatting.prettierd.with({
 		filetypes = {
